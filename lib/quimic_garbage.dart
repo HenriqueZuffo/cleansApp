ddb95d7f5165aad75821b03174371b1638956280
Henrique Gabriel de Andrade <henrique.andrade@wmcsistemas.com>
2024-09-30 21:08:50 -0300

    add bullet list lib

--- lib/quimic_garbage.dart
+++ lib/quimic_garbage.dart
@@ -1,5 +1,6 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:super_bullet_list/bullet_list.dart';
+import 'package:super_bullet_list/bullet_style.dart';
 
 class QuimicGarbage extends StatefulWidget {
   const QuimicGarbage({super.key});
@@ -18,45 +19,51 @@ class _QuimicGarbageState extends State<QuimicGarbage> {
       body:  Padding(
         padding: const EdgeInsets.all(16.0),
         child: ListView(
-          children: [
-            const Text(
+          children: const [
+            Text(
               'Lixo Químico/Industrial',
               style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
             ),
-            const SizedBox(height: 16),
-            const Text(
+            SizedBox(height: 17),
+            Text(
               'O lixo químico/industrial é composto por resíduos provenientes de fábricas, indústrias e laboratórios, como solventes, ácidos, metais pesados, pesticidas, plásticos e produtos de limpeza. Esses resíduos, quando descartados inadequadamente, podem provocar sérios danos à saúde e ao meio ambiente.',
-              style: TextStyle(fontSize: 16),
+              style: TextStyle(fontSize: 17),
             ),
-            const SizedBox(height: 16),
-            const Text(
+            SizedBox(height: 17),
+            Text(
               'Problemas de saúde causados pelo lixo químico/industrial:',
-              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
             ),
-            const SizedBox(height: 8),
-            ..._buildHealthProblems(),
+            SizedBox(height: 8),
+            SuperBulletList(
+                isOrdered: false,
+                items: [
+                  Text("Intoxicações agudas: Exposição a vapores tóxicos pode causar tontura, dor de cabeça, vômitos e irritações nas vias respiratórias.",
+                      style: TextStyle(fontSize: 17)
+                  ),
+                  Text("Problemas neurológicos: Metais pesados afetam o sistema nervoso central, levando a problemas cognitivos e perda de memória.",
+                      style: TextStyle(fontSize: 17)
+                  ),
+                  Text("Problemas respiratórios: Inalação de partículas químicas pode causar doenças respiratórias crônicas.",
+                      style: TextStyle(fontSize: 17)
+                  ),
+                  Text("Danos ao fígado e rins: Substâncias químicas podem causar hepatotoxicidade e nefrotoxicidade.",
+                      style: TextStyle(fontSize: 17)
+                  ),
+                  Text("Problemas reprodutivos e genéticos: Exposição a produtos químicos pode causar infertilidade e malformações congênitas.",
+                      style: TextStyle(fontSize: 17)
+                  ),
+                  Text("Câncer: Muitas substâncias químicas presentes no lixo industrial são carcinogênicas.",
+                      style: TextStyle(fontSize: 17)
+                  ),
+                  Text("Contaminação da água e alimentos: Resíduos industriais podem infiltrar-se no solo e contaminar fontes de água potável.",
+                      style: TextStyle(fontSize: 17)
+                  )
+                ]
+            )
           ],
         ),
       ),
     );
-  }
-
-  List<Widget> _buildHealthProblems() {
-    const healthProblems = [
-      "Intoxicações agudas: Exposição a vapores tóxicos pode causar tontura, dor de cabeça, vômitos e irritações nas vias respiratórias.",
-      "Problemas neurológicos: Metais pesados afetam o sistema nervoso central, levando a problemas cognitivos e perda de memória.",
-      "Problemas respiratórios: Inalação de partículas químicas pode causar doenças respiratórias crônicas.",
-      "Danos ao fígado e rins: Substâncias químicas podem causar hepatotoxicidade e nefrotoxicidade.",
-      "Problemas reprodutivos e genéticos: Exposição a produtos químicos pode causar infertilidade e malformações congênitas.",
-      "Câncer: Muitas substâncias químicas presentes no lixo industrial são carcinogênicas.",
-      "Contaminação da água e alimentos: Resíduos industriais podem infiltrar-se no solo e contaminar fontes de água potável.",
-    ];
-
-    return healthProblems
-        .map((problem) => Padding(
-            padding: const EdgeInsets.symmetric(vertical: 4.0),
-            child: Text('• $problem', style: TextStyle(fontSize: 16)),
-          )
-        ).toList();
   }
 }
