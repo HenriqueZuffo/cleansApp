b3eb261f3986fb07f499fc5026fa6ce548c816ca
Henrique Gabriel de Andrade <henrique.andrade@wmcsistemas.com>
2024-10-01 19:32:13 -0300

    feat: refactor hospital_garbage page

--- lib/hospital_garbage.dart
+++ lib/hospital_garbage.dart
@@ -1,5 +1,5 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:super_bullet_list/bullet_list.dart';
 
 class HospitalGarbage extends StatefulWidget {
   const HospitalGarbage({super.key});
@@ -15,35 +15,66 @@ class _HospitalGarbageState extends State<HospitalGarbage> {
       appBar: AppBar(
         title: const Text('Lixo Hospitalar ou Biomédico'),
       ),
-      body: const Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Row(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text('Exemplo: ', style: TextStyle(fontWeight: FontWeight.bold)),
-              Expanded(
-                child: Text('Agulhas, seringas, curativos, medicamentos vencidos.')
-              )
-            ],
+      body: ListView(
+        padding: const EdgeInsets.all(16.0),
+        children: const [
+          Text(
+            "Lixo Hospitalar ou Biomédico",
+            style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
           ),
-          SizedBox(height: 20,),
-          Row(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text('Danos à Saúde: ', style: TextStyle(fontWeight: FontWeight.bold),
-              ),
-              SizedBox(width: 10,),
-              Expanded(
-                  child: Text('Pode transmitir infecções graves, '
-                      'como hepatite e HIV, além de expor as pessoas a substâncias químicas tóxicas. '
-                      'O lixo hospitalar mal descartado coloca em risco a saúde dos trabalhadores de coleta e da '
-                      'população em geral.',
-                  ))
-            ],
+          SizedBox(height: 17),
+          Text(
+              "O lixo hospitalar consiste em resíduos gerados por hospitais, clínicas, laboratórios, consultórios médicos e odontológicos, como seringas, agulhas, luvas, tecidos, medicamentos vencidos, materiais infecciosos e objetos cortantes. Este tipo de lixo é considerado altamente perigoso devido ao risco biológico e químico que oferece.",
+            style: TextStyle(fontSize: 17),
+          ),
+          SizedBox(height: 17),
+          Text(
+            'Problemas de saúde e ao meio ambiente:',
+            style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+          ),
+          SizedBox(height: 8),
+          SuperBulletList(
+              items: [
+                Text(
+                  "Risco de infecções e doenças: Materiais contaminados, como seringas e agulhas usadas, podem transmitir doenças infecciosas graves, incluindo HIV, hepatite B e C, se manipulados de forma inadequada.",
+                  style: TextStyle(fontSize: 17),
+                ),
+                Text(
+                  "Exposição a agentes biológicos perigosos: O lixo hospitalar pode conter fluidos corporais, tecidos infectados e outros materiais que representam risco de contaminação para quem entra em contato com eles, como trabalhadores de saúde e de limpeza.",
+                  style: TextStyle(fontSize: 17),
+                ),
+                Text(
+                  "Intoxicação por produtos químicos: Medicamentos vencidos e produtos químicos hospitalares podem causar intoxicações se descartados de forma inadequada, levando a problemas como envenenamento e alergias graves.",
+                  style: TextStyle(fontSize: 17),
+                ),
+                Text(
+                  "Lesões por objetos cortantes: Materiais como bisturis, lâminas e vidros podem causar ferimentos que, além do dano físico, podem resultar em infecções severas.",
+                  style: TextStyle(fontSize: 17),
+                ),
+                Text(
+                  "Contaminação do solo e da água: O descarte inadequado de resíduos hospitalares pode resultar na infiltração de agentes biológicos e químicos no solo e na água subterrânea, contaminando ecossistemas e fontes de água potável.",
+                  style: TextStyle(fontSize: 17),
+                ),
+                Text(
+                  "Poluição do ar por incineração inadequada: A queima de resíduos hospitalares em fornos sem os devidos controles pode liberar gases tóxicos, como dioxinas e furanos, que poluem o ar e contribuem para problemas respiratórios nas comunidades ao redor.",
+                  style: TextStyle(fontSize: 17),
+                ),
+                Text(
+                  "Impacto na fauna e flora: A contaminação ambiental causada pelo lixo hospitalar pode prejudicar animais e plantas, afetando a biodiversidade e os ecossistemas locais.",
+                  style: TextStyle(fontSize: 17),
+                ),
+                Text(
+                  "Erosão da qualidade do solo: Produtos químicos e agentes patogênicos presentes no lixo hospitalar podem danificar o solo, tornando-o inadequado para a agricultura e contribuindo para a degradação ambiental.",
+                  style: TextStyle(fontSize: 17),
+                ),
+                Text(
+                  "Propagação de pragas: O lixo hospitalar mal gerido pode atrair pragas, como ratos e mosquitos, que podem ser vetores de doenças, agravando problemas de saúde pública.",
+                  style: TextStyle(fontSize: 17),
+                )
+              ]
           )
         ],
-      ),
+      )
     );
   }
 }
