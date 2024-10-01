62d7e4e88a333753915f71039b5486466c2b2319
Henrique Gabriel de Andrade <henrique.andrade@wmcsistemas.com>
2024-10-01 18:56:13 -0300

    refactor: refactor recycle_garbage

--- lib/quimic_garbage.dart
+++ lib/quimic_garbage.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:super_bullet_list/bullet_list.dart';
-import 'package:super_bullet_list/bullet_style.dart';
 
 class QuimicGarbage extends StatefulWidget {
   const QuimicGarbage({super.key});
@@ -16,53 +15,51 @@ class _QuimicGarbageState extends State<QuimicGarbage> {
       appBar: AppBar(
         title: const Text('Lixo Químico/Industrial'),
       ),
-      body:  Padding(
+      body: ListView(
         padding: const EdgeInsets.all(16.0),
-        child: ListView(
-          children: const [
-            Text(
-              'Lixo Químico/Industrial',
-              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-            ),
-            SizedBox(height: 17),
-            Text(
-              'O lixo químico/industrial é composto por resíduos provenientes de fábricas, indústrias e laboratórios, como solventes, ácidos, metais pesados, pesticidas, plásticos e produtos de limpeza. Esses resíduos, quando descartados inadequadamente, podem provocar sérios danos à saúde e ao meio ambiente.',
-              style: TextStyle(fontSize: 17),
-            ),
-            SizedBox(height: 17),
-            Text(
-              'Problemas de saúde causados pelo lixo químico/industrial:',
-              style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
-            ),
-            SizedBox(height: 8),
-            SuperBulletList(
-                isOrdered: false,
-                items: [
-                  Text("Intoxicações agudas: Exposição a vapores tóxicos pode causar tontura, dor de cabeça, vômitos e irritações nas vias respiratórias.",
-                      style: TextStyle(fontSize: 17)
-                  ),
-                  Text("Problemas neurológicos: Metais pesados afetam o sistema nervoso central, levando a problemas cognitivos e perda de memória.",
-                      style: TextStyle(fontSize: 17)
-                  ),
-                  Text("Problemas respiratórios: Inalação de partículas químicas pode causar doenças respiratórias crônicas.",
-                      style: TextStyle(fontSize: 17)
-                  ),
-                  Text("Danos ao fígado e rins: Substâncias químicas podem causar hepatotoxicidade e nefrotoxicidade.",
-                      style: TextStyle(fontSize: 17)
-                  ),
-                  Text("Problemas reprodutivos e genéticos: Exposição a produtos químicos pode causar infertilidade e malformações congênitas.",
-                      style: TextStyle(fontSize: 17)
-                  ),
-                  Text("Câncer: Muitas substâncias químicas presentes no lixo industrial são carcinogênicas.",
-                      style: TextStyle(fontSize: 17)
-                  ),
-                  Text("Contaminação da água e alimentos: Resíduos industriais podem infiltrar-se no solo e contaminar fontes de água potável.",
-                      style: TextStyle(fontSize: 17)
-                  )
-                ]
-            )
-          ],
-        ),
+        children: const [
+          Text(
+            'Lixo Químico/Industrial',
+            style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+          ),
+          SizedBox(height: 17),
+          Text(
+            'O lixo químico/industrial é composto por resíduos provenientes de fábricas, indústrias e laboratórios, como solventes, ácidos, metais pesados, pesticidas, plásticos e produtos de limpeza. Esses resíduos, quando descartados inadequadamente, podem provocar sérios danos à saúde e ao meio ambiente.',
+            style: TextStyle(fontSize: 17),
+          ),
+          SizedBox(height: 17),
+          Text(
+            'Problemas de saúde causados pelo lixo químico/industrial:',
+            style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+          ),
+          SizedBox(height: 8),
+          SuperBulletList(
+              isOrdered: false,
+              items: [
+                Text("Intoxicações agudas: Exposição a vapores tóxicos pode causar tontura, dor de cabeça, vômitos e irritações nas vias respiratórias.",
+                    style: TextStyle(fontSize: 17)
+                ),
+                Text("Problemas neurológicos: Metais pesados afetam o sistema nervoso central, levando a problemas cognitivos e perda de memória.",
+                    style: TextStyle(fontSize: 17)
+                ),
+                Text("Problemas respiratórios: Inalação de partículas químicas pode causar doenças respiratórias crônicas.",
+                    style: TextStyle(fontSize: 17)
+                ),
+                Text("Danos ao fígado e rins: Substâncias químicas podem causar hepatotoxicidade e nefrotoxicidade.",
+                    style: TextStyle(fontSize: 17)
+                ),
+                Text("Problemas reprodutivos e genéticos: Exposição a produtos químicos pode causar infertilidade e malformações congênitas.",
+                    style: TextStyle(fontSize: 17)
+                ),
+                Text("Câncer: Muitas substâncias químicas presentes no lixo industrial são carcinogênicas.",
+                    style: TextStyle(fontSize: 17)
+                ),
+                Text("Contaminação da água e alimentos: Resíduos industriais podem infiltrar-se no solo e contaminar fontes de água potável.",
+                    style: TextStyle(fontSize: 17)
+                )
+              ]
+          )
+        ],
       ),
     );
   }
--- lib/recycle_garbage.dart
+++ lib/recycle_garbage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:super_bullet_list/bullet_list.dart';
 
 class RecycleGarbage extends StatefulWidget {
   const RecycleGarbage({super.key});
@@ -14,30 +15,49 @@ class _RecycleGarbageState extends State<RecycleGarbage> {
       appBar: AppBar(
         title: const Text('Lixo reciclável'),
       ),
-      body: const Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Row(
-            children: [
-              Text('Exemplo: ', style: TextStyle(fontWeight: FontWeight.bold)),
-              Text('Papéis, plásticos, vidros, metais.')
-            ],
+      body: ListView(
+        padding: const EdgeInsets.all(16.0),
+        children: const [
+          Text(
+            "Lixo reciclável",
+            style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
           ),
-          SizedBox(height: 20,),
-          Row(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text('Danos à Saúde: ', style: TextStyle(fontWeight: FontWeight.bold),
-              ),
-              SizedBox(width: 10,),
-              Expanded(
-                  child: Text('Não é diretamente perigoso à saúde se manuseado corretamente. No entanto, '
-                      'quando acumulado de forma inadequada, pode acumular água e servir como criadouro de'
-                      ' mosquitos, como o Aedes aegypti, que transmite doenças como a dengue e chikungunya',
-                  ))
-            ],
+          SizedBox(height: 17),
+          Text(
+              "O lixo reciclável é composto por materiais que podem ser reprocessados e reutilizados para "
+              "fabricar novos produtos. Entre os principais tipos de lixo reciclável estão o papel, o plástico, o vidro e o metal. A reciclagem contribui para a redução do consumo de recursos naturais, diminui a quantidade de resíduos descartados em aterros e minimiza os impactos ambientais causados pela produção de novos materiais. Para ser reciclado de forma eficiente, o lixo deve ser separado e descartado adequadamente, facilitando o processo de reutilização.",
+              style: TextStyle(fontSize: 17)
+          ),
+          SizedBox(height: 17),
+          Text(
+            'Problemas de saúde e ao meio ambiente:',
+            style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+          ),
+          SizedBox(height: 17),
+          SuperBulletList(
+              items: [
+                Text("Acúmulo de resíduos em aterros sanitários: O lixo reciclável, quando descartado incorretamente, ocupa espaço nos aterros, aumentando a poluição do solo e da água.",
+                    style: TextStyle(fontSize: 17)
+                ),
+                Text("Poluição por plásticos: Plásticos descartados inadequadamente podem demorar centenas de anos para se decompor, poluindo o ambiente e gerando microplásticos.",
+                    style: TextStyle(fontSize: 17)
+                ),
+                Text("Risco para a fauna: Animais podem ingerir resíduos plásticos ou se enroscar em materiais recicláveis, o que pode levar à morte ou causar danos à fauna.",
+                    style: TextStyle(fontSize: 17)
+                ),
+                Text("Contaminação da água: Resíduos não reciclados corretamente podem chegar aos rios, lagos e oceanos, afetando a qualidade da água e a vida aquática.",
+                    style: TextStyle(fontSize: 17)
+                ),
+                Text("Obstrução de sistemas de drenagem: Materiais como plásticos e papéis podem obstruir redes de esgoto e drenagem, causando enchentes e agravando problemas urbanos.",
+                    style: TextStyle(fontSize: 17)
+                ),
+                Text("Impacto na cadeia alimentar: Microplásticos resultantes da degradação de materiais recicláveis podem ser ingeridos por organismos marinhos, entrando na cadeia alimentar e afetando a saúde de animais e seres humanos.",
+                    style: TextStyle(fontSize: 17)
+                ),
+              ]
           )
         ],
+
       ),
     );
   }
